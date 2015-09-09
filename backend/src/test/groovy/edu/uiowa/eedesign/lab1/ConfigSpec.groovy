7350b918d14b47f90c6ebb9275ca1e0d995880ac
Jake Klinker <jklinker1@gmail.com>
2015-09-09 10:58:42 -0500

    remove tests

--- backend/src/test/groovy/edu/uiowa/eedesign/lab1/ConfigSpec.groovy
+++ backend/src/test/groovy/edu/uiowa/eedesign/lab1/ConfigSpec.groovy
@@ -14,9 +14,4 @@ class ConfigSpec extends Specification {
 
     def cleanup() {
     }
-
-    void "test something"() {
-        expect:"fix me"
-            true == false
-    }
 }
--- backend/src/test/groovy/edu/uiowa/eedesign/lab1/TemperatureControllerSpec.groovy
+++ backend/src/test/groovy/edu/uiowa/eedesign/lab1/TemperatureControllerSpec.groovy
@@ -14,9 +14,4 @@ class TemperatureControllerSpec extends Specification {
 
     def cleanup() {
     }
-
-    void "test something"() {
-        expect:"fix me"
-            true == false
-    }
 }
